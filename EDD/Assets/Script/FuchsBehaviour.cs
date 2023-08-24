0ed9cd3598822a83abea8ada1d6ff6ad2272e33b
KlaraLindner <klara.lindner@stud.htwk-leipzig.de>
2023-08-24 18:41:33 +0200

    Added Fox-animation and movement changes

--- EDD/Assets/Script/FuchsBehaviour.cs
+++ EDD/Assets/Script/FuchsBehaviour.cs
@@ -2,19 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FuchsBehaviour : MonoBehaviour
+public class FuchsBehaviour : RandomAnimalMovement
 {
     [SerializeField] Animator fuchsAnimator;
-    [Range(0.25f, 10f)]
-    [SerializeField] float moveSpeed = 0.1f;
     // Start is called before the first frame update
     void Start()
     {
         if (!fuchsAnimator)
             Debug.LogError("No animator Linked, animatons wont work properly!");
     }
-    void Update(){
-        transform.Translate(0f, 0f,moveSpeed*Time.deltaTime);
+    protected override void Update()
+    {
+        base.Update();
+        Debug.Log(" "+currentDir);
+        Vector3 currentLerpDir =Vector3.Lerp(transform.forward, currentDir, Time.deltaTime);
+        Debug.Log("Angle "+Vector3.SignedAngle(currentLerpDir, transform.forward, Vector3.up));
+
+        fuchsAnimator.SetFloat("Turn", Vector3.SignedAngle(currentDir, transform.forward, Vector3.up));
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z);
+        // fuchsAnimator.SetFloat("Turn", );
     }
     // Update is called once per frame
 
--- EDD/Assets/Script/RandomAnimalMovement.cs
+++ EDD/Assets/Script/RandomAnimalMovement.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 public class RandomAnimalMovement : MonoBehaviour
 {
-    [SerializeField] RandomAnimalMovement rAM;
+
     [SerializeField] Collider[] pathPoints;
     [SerializeField] [Range(0.01f,1f)] float movementSpeed;
     [SerializeField] int currentIndexPathPoint = 0;
+    [SerializeField] bool lockPosition = false;
+    [SerializeField] bool lockRotation = false;
+    protected Vector3 currentDir = Vector3.zero;
+
+
     void Start()
     {
-        rAM = this;
         if (pathPoints == null)
         {
             Debug.LogError("No paths assignded, path finding wont work!");
@@ -17,23 +21,23 @@ public class RandomAnimalMovement : MonoBehaviour
         }
     }
 
-
-    void Update()
+   protected virtual void Update()
     {
-        Vector3 currentDir = pathPoints[currentIndexPathPoint].transform.position - transform.position;
-        transform.Translate((Vector3.forward*Time.deltaTime*movementSpeed) * currentDir.magnitude);
-       
-        transform.forward = Vector3.Lerp(transform.forward, currentDir, Time.deltaTime);
       
+         currentDir = pathPoints[currentIndexPathPoint].transform.position - transform.position;
+        if (!lockPosition)
+            transform.Translate(currentDir.magnitude * movementSpeed * Time.deltaTime * Vector3.forward);
        
+        if (!lockRotation)
+           transform.forward = Vector3.Lerp(transform.forward, currentDir, Time.deltaTime);
     }
+          
 
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("On Trigger Enter called "+ pathPoints[currentIndexPathPoint].transform.Equals(other.transform));
         if (!pathPoints[currentIndexPathPoint].transform.Equals(other.transform))
                 return;
-
             currentIndexPathPoint++;
         if (currentIndexPathPoint > pathPoints.Length-1)
             currentIndexPathPoint = 0;
