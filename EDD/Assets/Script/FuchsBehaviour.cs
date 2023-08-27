108c8ba2b7944e4fdf5989d86d23cfede294aee0
KlaraLindner <klara.lindner@stud.htwk-leipzig.de>
2023-08-27 18:43:23 +0200

    Corrected Fox beahviour

--- EDD/Assets/Script/FuchsBehaviour.cs
+++ EDD/Assets/Script/FuchsBehaviour.cs
@@ -17,16 +17,16 @@ public class FuchsBehaviour : RandomAnimalMovement
     {
         if (!fuchsAnimator)
             Debug.LogError("No animator Linked, animatons wont work properly!");
-        startDir=transform.TransformDirection(0, -0.5f, 0);
+        startDir=transform.TransformDirection(0, -1f, 1f);
     }
-     void LateUpdate()
+    protected override void Update()
     {
         base.Update();
-       
-        localDir =  startDir;
+
+        localDir = startDir;
         rayoffset = new Vector3(transform.position.x, transform.position.y+1, transform.position.z + 1f);
         Vector2 twoAxisforward = new Vector2(transform.forward.x, transform.forward.z);
-        if (Physics.Raycast(rayoffset, localDir, out hit, 3f,1<<3))
+        if (Physics.Raycast(rayoffset, localDir, out hit, 5f,1<<3))
         {
             Debug.Log("Hitahead!");
             hitAhead = new Vector2(hit.point.y, hit.point.z);
@@ -35,12 +35,10 @@ public class FuchsBehaviour : RandomAnimalMovement
             Vector3 changeingVector = new Vector3(transform.forward.x, hit.point.y - transform.position.y, transform.forward.z);
 
             Vector2 twoAxisDir = new Vector2(currentDir.x, currentDir.z);
-            Mathf.LerpAngle(transform.localEulerAngles.x, Vector2.SignedAngle(twoAxisDir, twoAxisforward), Time.deltaTime);
+           float trundir= Mathf.LerpAngle(transform.localEulerAngles.y, Vector2.SignedAngle(twoAxisDir, twoAxisforward), Time.deltaTime);
             
-            float rotX = Mathf.LerpAngle(transform.localEulerAngles.x, Vector3.SignedAngle(transform.forward, changeingVector, Vector3.right), Time.deltaTime);
-           
-            
-            transform.rotation = Quaternion.Euler(rotX, transform.eulerAngles.y, transform.eulerAngles.z);
+            float rotX = Mathf.LerpAngle(transform.localEulerAngles.x, Vector3.SignedAngle(transform.forward, changeingVector, transform.right), Time.deltaTime);
+            transform.rotation = Quaternion.Euler(rotX,trundir, 0f);
 
         }
        
@@ -62,15 +60,16 @@ public class FuchsBehaviour : RandomAnimalMovement
     // Update is called once per frame
     private void OnDrawGizmos()
     {
-        startDir = transform.TransformDirection(0, -0.5f, 0.5f);
+     
        rayoffset= new Vector3(transform.position.x, transform.position.y + 1, transform.position.z + 1f);
         Gizmos.DrawRay(transform.position, transform.forward);
         Gizmos.color= Color.black;
         if (Physics.Raycast(rayoffset, startDir, out hit, 3f, 1 << 3))
         { Vector3 changeingVector = new Vector3(transform.forward.x,   hit.point.y- transform.position.y, transform.forward.z);
           
-            Gizmos.DrawRay(transform.position, changeingVector);
-            
+            Gizmos.DrawRay(transform.position, changeingVector*3);
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(rayoffset, startDir);
            
         }
     }
